a933d80b1d12a41dddfdcaeeccf1924712aa609d
MountainGator <luketaylorsanders@gmail.com>
2022-02-04 13:17:43 -0600

    Solve entire project, plus added extras

---
+++ js/main.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// So we don't have to keep re-finding things on page, find DOM elements once:
+
+const $body = $("body");
+
+const $storiesLoadingMsg = $("#stories-loading-msg");
+const $allStoriesList = $("#all-stories-list");
+
+const $loginForm = $("#login-form");
+const $signupForm = $("#signup-form");
+
+const $navLogin = $("#nav-login");
+const $navUserProfile = $("#nav-user-profile");
+const $navLogOut = $("#nav-logout");
+$navLogOut.hide();
+$navUserProfile.hide();
+
+const $gamescores = $('#gamescores');
+const $pastGames = $('#pastGames');
+const $futureGames = $('#futureGames');
+const $articleList = $('#article-list');
+const $newStories = $('#newStories');
+$newStories.hide();
+
+const $addMe = $('#add-me');
+const $submitMe = $('#submit-me');
+const $allowDelete = $('#allow-delete');
+const $submitNewStory = $('#submit-new-story');
+$addMe.hide();
+
+const currDay = new Date();
+const nextDay = new Date(currDay);
+
+nextDay.setDate(nextDay.getDate() + 1);
+
+let day = nextDay.getDate();
+let yesterday = currDay.getDate();
+let month = nextDay.getMonth();
+month += 1;
+let year = nextDay.getFullYear();
+
+if(day < 10) { day = `0${day}`;}
+if(yesterday < 10) { yesterday = `0${yesterday}`;}
+if(month < 10) {month = `0${month}`;}
+
+let format = `${year}-${month}-${day}`;
+let pastFormat = `${year}-${month}-${yesterday}`;
+
+/** To make it easier for individual components to show just themselves, this
+ * is a useful function that hides pretty much everything on the page. After
+ * calling this, individual components can re-show just what they want.
+ */
+
+function hidePageComponents() {
+  const components = [
+    $allStoriesList,
+    $loginForm,
+    $signupForm,
+    $gamescores,
+    $articleList,
+  ];
+  components.forEach(c => c.hide());
+}
+
+/** Overall function to kick off the app. */
+
+
+
+async function start() {
+  console.debug("start");
+  // "Remember logged-in user" and log in, if credentials in localStorage
+  await checkForRememberedUser();
+  await getAndShowStoriesOnStart();
+  await getPastGames();
+  await getFutureGames();
+  await getTopStories();
+  // if we got a logged-in user
+  if (currentUser) {
+    updateUIOnUserLogin();
+  }
+}
+
+// Once the DOM is entirely loaded, begin the app
+  $(start);
+
+
---
+++ js/stories.js
@@ -0,0 +1,289 @@
+"use strict";
+
+let storyList;
+let click = 1;
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+function generateStoryMarkup(story, count) {
+  if(currentUser){
+    return $(
+        ` <div class="col-md-6 col-lg-3 mt-2">
+          <div class="card text-dark bg-light">
+            <div class="card-body" id="${story.storyId}">
+              <h5 class="card-title">${story.title}</h5>
+              <p class="card-text">By ${story.author}</p>
+              <p class="card-text">posted by ${story.username}</p>
+              <a href="${story.url}" class="btn btn-primary" target="_blank">Read</a>
+              <a type="button" class="btn btn-success select" id="f${count}">Favorite</a>
+            </div>
+          </div>
+        </div>
+      `);
+  } else return $(
+    ` <div class="col-md-6 col-lg-3 mt-2">
+      <div class="card text-dark bg-light">
+        <div class="card-body" id="${story.storyId}">
+          <h5 class="card-title">${story.title}</h5>
+          <p class="card-text">By ${story.author}</p>
+          <p class="card-text">posted by ${story.username}</p>
+          <a href="${story.url}" class="btn btn-dark" target="_blank">Read</a>
+        </div>
+      </div>
+    </div>
+  `);
+}
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+  let count = 0;
+
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story, count);
+    $allStoriesList.append($story);
+    count ++;
+  }
+  $allStoriesList.show();
+}
+
+async function getPastGames () {
+  const options = {
+    method: 'GET',
+    url: `https://api-nba-v1.p.rapidapi.com/games/date/${pastFormat}`,
+    headers: {
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': '1de9427720msh6e2bc8a9e101267p1eb3cdjsnfb435a5f3e89'
+    }
+  };
+  const response = await axios.request(options);
+  const results = response.data;
+  const gamesArr = results.api.games;
+  let gameDetail = [];
+
+  for(let game of gamesArr) {
+
+    let { gameId, hTeam, vTeam } = game; 
+    gameDetail.push({gameId, hTeam, vTeam})
+  }
+
+  makePastBoard(gameDetail);
+}
+
+async function makePastBoard(games) {
+  const gamesArr = await games;
+
+  for(let game of gamesArr) {
+    let $gameRow = $(`
+      <tr id="${game.gameId}">  
+        <td>
+          <img src="${game.hTeam.logo}" alt="home team logo" height="20">
+          ${game.hTeam.nickName}
+        </td>
+        <td>
+          ${game.hTeam.score.points}
+        </td>
+        <td>
+          <img src="${game.vTeam.logo}" alt="home team logo" height="20">
+          ${game.vTeam.nickName}
+        </td>
+        <td>
+          ${game.vTeam.score.points}
+        </td>
+      </tr>
+    `);
+
+    $pastGames.append($gameRow);
+  }
+}
+
+async function getFutureGames () {
+  const options = {
+    method: 'GET',
+    url: `https://api-nba-v1.p.rapidapi.com/games/date/${format}`,
+    headers: {
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+      'x-rapidapi-key': '1de9427720msh6e2bc8a9e101267p1eb3cdjsnfb435a5f3e89'
+    }
+  };
+  
+  const response = await axios.request(options);
+  const results = response.data;
+  let gamesArr = results.api.games;
+  let gameDetail = [];
+
+  for(let game of gamesArr) {
+
+    let { gameId, hTeam, vTeam, startTimeUTC } = game; 
+    gameDetail.push({gameId, hTeam, vTeam, startTimeUTC})
+  }
+
+  makeFutureBoard(gameDetail);
+}
+
+async function makeFutureBoard(games) {
+  const gamesArr = await games;
+
+  for(let game of gamesArr) {
+    let gameTime = new Date(game.startTimeUTC);
+    let localTime = gameTime.toLocaleTimeString('en-US');
+
+    let $gameRow = $(`
+      <tr id="${game.gameId}">  
+        <td>
+          <img src="${game.hTeam.logo}" alt="home team logo" height="20">
+          ${game.hTeam.nickName}
+        </td>
+        <td>
+          VS
+        </td>
+        <td>
+          <img src="${game.vTeam.logo}" alt="home team logo" height="20">
+          ${game.vTeam.nickName}
+        </td>
+        <td>
+          ${localTime}
+        </td>
+      </tr>
+    `);
+    $futureGames.append($gameRow);
+  }
+}
+
+async function getTopStories () {
+  const response = await axios.get(`https://newsapi.org/v2/everything?q=nba&from=${pastFormat}&to${format}&language=en&apiKey=f9d7a705eee0494d88a78a0b675a944a`);
+  const results = response.data;
+  const { articles } = results;
+
+  putNewsOnPage(articles);
+}
+
+async function putNewsOnPage (articles) {
+  const newsArr = await articles;
+  
+  for(let article of newsArr) {
+    let time = new Date(article.publishedAt);
+    let localTime = time.toString().slice(0,21);
+    
+    let $item = $(
+        `<div class="col-md-6 col-lg-3 mt-3">
+          <div class="card text-white bg-dark">
+            <div class="card-body">
+              <h5 class="card-title">${article.title}</h5>
+              <p class="card-text">On ${localTime}</p>
+              <p class="card-text">${article.description}</p>
+              <a href="${article.url}" class="btn btn-warning" target="_blank">Read More</a>
+            </div>
+          </div>
+        </div>`);
+      
+      $articleList.append($item); 
+  }
+}
+
+$('#all-stories-list').on('click', '.select', favoriteStory);
+
+async function favoriteStory (e) {
+  e.preventDefault();
+  const $favBtn = $(e.target);
+  const $storyDiv = $favBtn.closest('div');
+  const storyId = $storyDiv.attr('id');
+  const story = storyList.stories.find(s => s.storyId === storyId);
+
+
+  if ($storyDiv.hasClass("fav")) {
+    await currentUser.removeFavorite(story);
+    $storyDiv.removeClass("fav");
+    $storyDiv.parent().attr("class", "card text-dark bg-light");
+    $favBtn.attr("class", "btn btn-success select");
+    $favBtn.text('Favorite');
+  } else {
+    await currentUser.addFavorite(story);
+    $storyDiv.addClass("fav");
+    $storyDiv.parent().attr("class", "card text-dark bg-warning");
+    $favBtn.attr("class", "btn btn-danger select");
+    $favBtn.text('Unfavorite');
+  }
+}
+
+function checkForFavorites (array) {
+  for(let story of array) {
+    let allBtns = $('.select');
+    let $div = $(`#${story.storyId}`);
+    $div.parent().attr("class", "card text-dark bg-warning");
+    $div.attr("class", "card-body fav");
+    $div.find(allBtns).attr("class", "btn btn-danger select").text('Unfavorite');
+  }
+}
+
+$submitMe.on('click', e => {
+  e.preventDefault();
+  hidePageComponents();
+  $addMe.show();
+})
+
+$('#new-story-form').on('submit', submitStory);
+
+async function submitStory (evt) {
+  evt.preventDefault();
+  console.debug(submitStory);
+  const $title = $('#title').val();
+  const $author = $('#author-input').val();
+  const $url = $('#url-input').val();
+
+  if(!$title || !$author || !$url) {
+    alert('Please enter a title, author name, and url');
+  }
+
+  await storyList.addStory(currentUser, { "title": $title, "author": $author, "url": $url });
+
+  updateUIOnUserLogin();
+}
+
+$allowDelete.on('click', addDeleteBtn);
+
+async function addDeleteBtn () {
+  
+  if (click === 2) {
+    for(let story of await storyList.stories) {
+      if (story.username === currentUser.username) {
+        $(`#${story.storyId}`).find('.delete-me').remove();
+        $(`#${story.storyId}`).find('.select').show();
+      }
+    }
+    $allowDelete.text('Turn On Delete');
+  } else if (click === 1){
+    for(let story of await storyList.stories) {
+      if (story.username === currentUser.username) {
+        $(`#${story.storyId}`).find('.select').hide();
+        let $delBtn = $('<a type="button" class="btn btn-danger delete-me">Delete!</a>');
+        $(`#${story.storyId}`).append($delBtn);
+      }
+    }
+    $allowDelete.text('Turn Off Delete');
+  } 
+
+  click = click === 1 ? 2 : 1;
+}
+
+$allStoriesList.on('click', '.delete-me', deleteStory)
+
+async function deleteStory(e) {
+  e.preventDefault();
+  const $tgtBtn = $(e.target);
+  const $div = $tgtBtn.closest('div');
+  const $storyId = $div.attr('id');
+
+  await storyList.removeStory(currentUser, $storyId); 
+  
+  $div.parent().remove();
+  updateUIOnUserLogin();
+  addDeleteBtn();
+}
+
